7ad2c809bc1d33c71b3bce8b8f6cebaf5234885c
Víctor López <vlopez.gal@gmail.com>
2017-10-07 12:46:20 +0200

    Add boostrap dependency

--- page/bici-stats/src/App.js
+++ page/bici-stats/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import Hello from './components/hello';
 import './App.css';
 
 class App extends Component {
@@ -13,6 +14,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Hello nombre={"Jaimito"}/>
       </div>
     );
   }
