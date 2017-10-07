abb788e676ab013fbe3659313add044cb1388b14
Víctor López <vlopez.gal@gmail.com>
2017-10-07 18:55:37 +0200

    Graficos brutos

--- page/bici-stats/src/components/grafico.js
+++ page/bici-stats/src/components/grafico.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
-import {Thumbnail} from 'react-bootstrap';
+import {Thumbnail, Col} from 'react-bootstrap';
 
 export default class Grafico extends Component {
     render() {
+        const {titulo, descripcion, imagen} = this.props;
         return(
-            <Thumbnail src="/assets/pepa.jpeg" alt="242x200">
-                <h3>Thumbnail label</h3>
-                <p>Description</p>
-            </Thumbnail>
+            <Col xs={6} md={6}>
+                <Thumbnail src={imagen} alt="242x200">
+                    <h3>{titulo}</h3>
+                    <p>{descripcion}</p>
+                </Thumbnail>
+            </Col>
         )
     }
 };
--- page/bici-stats/src/components/graficos.js
+++ page/bici-stats/src/components/graficos.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react';
 import Grafico from './grafico';
+import {Grid,Row} from 'react-bootstrap';
+import img1 from '../assests/pepa.jpeg'
+import img2 from '../assests/pepa.jpeg'
+import img3 from '../assests/pepa.jpeg'
+import img4 from '../assests/pepa.jpeg'
+import img5 from '../assests/pepa.jpeg'
+import img6 from '../assests/pepa.jpeg'
 
 export default class Graficos extends Component {
-
     render() {
         return (
             <div>
-                    <Grafico/>
+                <Grid>
+                    <Row>
+                            <Grafico titulo={"aa"} descripcion={"descripcion"} imagen={img1} />
+                            <Grafico titulo={"aa"} descripcion={"descripcion"} imagen={img2} />
+                            <Grafico titulo={"aa"} descripcion={"descripcion"} imagen={img3} />
+                            <Grafico titulo={"aa"} descripcion={"descripcion"} imagen={img4} />
+                            <Grafico titulo={"aa"} descripcion={"descripcion"} imagen={img5} />
+                            <Grafico titulo={"aa"} descripcion={"descripcion"} imagen={img6} />
+
+                    </Row>
+                </Grid>
             </div>
         );
     }
