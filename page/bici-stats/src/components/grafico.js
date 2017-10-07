adb0580291644ab04c57416b6024b71ef0378df9
Víctor López <vlopez.gal@gmail.com>
2017-10-07 19:08:15 +0200

    Cambia tamaño thumbnail a todo el ancho

--- page/bici-stats/src/components/grafico.js
+++ page/bici-stats/src/components/grafico.js
@@ -5,7 +5,7 @@ export default class Grafico extends Component {
     render() {
         const {titulo, descripcion, imagen} = this.props;
         return(
-            <Col xs={6} md={6}>
+            <Col xs={6} md={12}>
                 <Thumbnail src={imagen} alt="242x200">
                     <h3>{titulo}</h3>
                     <p>{descripcion}</p>
